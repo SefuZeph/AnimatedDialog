15e151cbc17af61e4730391f1713a05a54a8facf
sefu <zephaniahsefuoduori@gmail.com>
2020-05-27 20:10:58 +0300

    Add library to application for use

--- app/src/main/java/com/sefu/animateddialog/MainActivity.kt
+++ app/src/main/java/com/sefu/animateddialog/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.sefu.animateddialog
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val animatedMaterialDialog: MaterialDialog = MaterialDialog.Builder(this)
+            .setTitle("")
+            .setMessage("")
+            .setCancelable(false)
+            .setAnimation("delete_anim.json")
+            .build()
+
+        animatedMaterialDialog.show()
     }
 }
